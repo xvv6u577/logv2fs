ee7321f5fd52903410528e736f76dcf6d3434071
caster8013 <11721318+caster8013@users.noreply.github.com>
2023-01-25 12:19:11 +0800

    traffic data UI improved at user according list.

--- frontend/src/components/trafficTable.js
+++ frontend/src/components/trafficTable.js
@@ -25,13 +25,14 @@ function TrafficTable({ data, limit, by }) {
 										<td className="py-4 px-2">{formatBytes(item.amount)}</td>
 										<td className="py-4 px-2">{item.used_by_domain && Object.entries(item.used_by_domain).map(([key, value]) => {
 											return (
-											<div key={key}>
-												<span className="d-block"><span className="inline-block w-44">{key}</span>:{" "}
-													<span className="inline-flex justify-center w-24 bg-green-100 text-green-800 text-sm font-medium mr-2 px-0 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
-														{formatBytes(value)}
+												<div key={key}>
+													<span className="d-block"><span className="inline-block w-44">{key}</span>:{" "}
+														<span className="inline-flex justify-center w-24 bg-green-100 text-green-800 text-sm font-medium mr-2 px-0 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+															{formatBytes(value)}
+														</span>
 													</span>
-												</span>
-											</div>)
+												</div>
+											)
 										})}
 										</td>
 									</tr>
@@ -46,7 +47,23 @@ function TrafficTable({ data, limit, by }) {
 										<td className="py-4 px-2">{item.period}</td>
 										<td className="py-4 px-2">{formatBytes(item.amount)}</td>
 										<td className="py-4 px-2">{item.amount && Object.entries(item.used_by_domain).map(([key, value]) => {
-											return (<div key={key}><span className="d-block">{key}:{" "}<span className="bg-green-100 text-green-800 text-sm font-medium mr-2 px-0 py-0.5 rounded dark:bg-green-200 dark:text-green-900">{formatBytes(value)}</span></span><br /></div>)
+											return (
+												// <div key={key}>
+												// 	<span className="d-block">
+												// 		{key}:{" "}
+												// 		<span className="bg-green-100 text-green-800 text-sm font-medium mr-2 px-0 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+												// 			{formatBytes(value)}
+												// 		</span>
+												// 	</span><br />
+												// </div>
+												<div key={key}>
+													<span className="d-block"><span className="inline-block w-44">{key}</span>:{" "}
+														<span className="inline-flex justify-center w-24 bg-green-100 text-green-800 text-sm font-medium mr-2 px-0 py-0.5 rounded dark:bg-green-200 dark:text-green-900">
+															{formatBytes(value)}
+														</span>
+													</span>
+												</div>
+											)
 										})}
 										</td>
 									</tr>
