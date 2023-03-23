fd8a0d985495a1e018f5e657b399098f18fc5aed
caster8013 <11721318+caster8013@users.noreply.github.com>
2023-03-23 20:51:12 +0800

    set add node model width to max-w-lg, fix remarks, domain name overflow.

--- frontend/src/components/addNode.js
+++ frontend/src/components/addNode.js
@@ -81,7 +81,7 @@ const AddNode = ({ btnName }) => {
 					id="add-node-modal"
 					className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex"
 				>
-					<div className="relative p-4 w-full max-w-md h-full md:h-auto">
+					<div className="relative p-4 w-full max-w-lg h-full md:h-auto">
 						<div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
 							<button
 								type="button"
