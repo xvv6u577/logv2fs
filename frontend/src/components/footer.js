0fddba40ec80c9dc979d9322cba711ab45dc3c1c
caster8013 <11721318+caster8013@users.noreply.github.com>
2023-05-27 22:49:24 +0800

    footer date updated. document title updated.

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -3,7 +3,7 @@ function Footer() {
 		<footer className="mt-auto py-3 bg-dark">
 			<div className="flex content-center justify-center">
 				<span className="text-muted text-light">
-					logv2rayfullstack <span>&copy;</span> 2022
+					Logv2 App <span>&copy;</span> 2023
 				</span>
 			</div>
 		</footer>
